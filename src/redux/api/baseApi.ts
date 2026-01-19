70d22b000e3eb9bf7ce8a7ba154ac48b8f6a3515
anisulhaqueniloy <niloy2931@gmail.com>
2026-01-20 01:12:30 +0600

    base api changed

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.wtapphub.com/api",
-    // baseUrl: "http://localhost:5000/api",
+
     credentials: "include",
 
     // prepareHeaders: (headers) => {
